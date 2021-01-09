e556ee4de5c63426ce07ce2d9371dd974b44b3bc
gkbamrah <guramritb@gmail.com>
2021-01-09 16:32:42 -0700

    added import statements to MainPage.js, and Matches.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./LandingPage.css";
 
-class LandingPage extends React.Component {
-    render() {
-        return (
-            <div className="App">
-            <header className="App-header">
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                
-                </p>
-                Learn React
-            </header>
-            </div>
-        )
-    }
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          Learn React
+        </header>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
