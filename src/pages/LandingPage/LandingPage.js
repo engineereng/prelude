e3c54fa820ee2df2a30066ed1ac830561ca2ba3a
Collin Eng <eng.engineereng@gmail.com>
2021-01-09 16:55:59 -0800

    Adding LandingPage css and components

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import "./LandingPage.css";
+import logo from "../../assets/logo512.png";
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 class LandingPage extends Component {
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          Learn React
-        </header>
+        {//<img id="logo" alt="prelude logo" src={logo} />
+        }
+        <div id="logo">placeholder logo</div>
+        <p id="left">
+          <h1>prelude</h1>
+          <p id="tagLine"><b>the beginning of your next chapter</b></p>
+          <p id="intro">let the music help you find your forever dance partner</p>
+        </p>
+        <div id="buttons">
+          <Button id="button">sign up</Button>
+          <Button>sign in</Button>
+        </div>
       </div>
     );
   }
