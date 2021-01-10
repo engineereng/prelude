f1e7d08ce75ee772b2b88e419e1cf996108a9464
Collin Eng <eng.engineereng@gmail.com>
2021-01-10 11:45:53 -0800

    Adding NotLoggedIn

---
+++ src/pages/NotLoggedIn/NotLoggedIn.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class NotLoggedIn extends Component {
+    render() {
+        return(<div>
+            You are not logged in.
+        </div>);
+    }
+}
+
+export default NotLoggedIn;
